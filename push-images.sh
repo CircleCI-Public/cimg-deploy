ee1be9190855ccfd402a52ac1fa5b6cd3682c815
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-03-30 20:48:12 -0400

    Initial commit [release]

---
+++ push-images.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
+
+docker push cimg/deploy:2022.04.1
+docker push cimg/deploy:2022.04
+docker push cimg/deploy:2022.04.1-node
+docker push cimg/deploy:2022.04-node

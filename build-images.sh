92e8c24401d0fe56713dba0d840bcef7466ac7f8
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-04-29 13:08:26 -0400

    Install AWS & GCP related tools (#1)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.04/Dockerfile -t cimg/deploy:2022.04.1  -t cimg/deploy:2022.04 .
-docker build --file 2022.04/node/Dockerfile -t cimg/deploy:2022.04.1-node  -t cimg/deploy:2022.04-node .
+docker build --file 2022.04/Dockerfile -t cimg/deploy:2022.04.2  -t cimg/deploy:2022.04 .
+docker build --file 2022.04/node/Dockerfile -t cimg/deploy:2022.04.2-node  -t cimg/deploy:2022.04-node .

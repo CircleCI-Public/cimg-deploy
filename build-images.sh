47488c1fa183754b294089f86b5180f7fd99b4eb
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-05-23 12:33:27 -0400

    Optimzie Azure section a bit (#3)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.04/Dockerfile -t cimg/deploy:2022.04.2  -t cimg/deploy:2022.04 .
-docker build --file 2022.04/node/Dockerfile -t cimg/deploy:2022.04.2-node  -t cimg/deploy:2022.04-node .
+docker build --file 2022.05/Dockerfile -t cimg/deploy:2022.05.1  -t cimg/deploy:2022.05 .
+docker build --file 2022.05/node/Dockerfile -t cimg/deploy:2022.05.1-node  -t cimg/deploy:2022.05-node .

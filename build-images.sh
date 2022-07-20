525e44138fe3454c3673b9e607bb46b11ffe1efc
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-07-20 10:51:54 -0400

    Publish v2022.07.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.06/Dockerfile -t cimg/deploy:2022.06.1 -t cimg/deploy:2022.06 .
-docker build --file 2022.06/node/Dockerfile -t cimg/deploy:2022.06.1-node -t cimg/deploy:2022.06-node .
+docker build --file 2022.07/Dockerfile -t cimg/deploy:2022.07.1 -t cimg/deploy:2022.07 .
+docker build --file 2022.07/node/Dockerfile -t cimg/deploy:2022.07.1-node -t cimg/deploy:2022.07-node .

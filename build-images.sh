157b11ee193e6d68fcf6826b4a866df94b7a0f75
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-09-15 20:56:27 -0400

    Publish v2022.09.1. [release] (#14)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.08/Dockerfile -t cimg/deploy:2022.08.1 -t cimg/deploy:2022.08 .
-docker build --file 2022.08/node/Dockerfile -t cimg/deploy:2022.08.1-node -t cimg/deploy:2022.08-node .
+docker build --file 2022.09/Dockerfile -t cimg/deploy:2022.09.1 -t cimg/deploy:2022.09 .
+docker build --file 2022.09/node/Dockerfile -t cimg/deploy:2022.09.1-node -t cimg/deploy:2022.09-node .
--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/deploy:2022.08
-docker push cimg/deploy:2022.08.1
-docker push cimg/deploy:2022.08-node
-docker push cimg/deploy:2022.08.1-node
+docker push cimg/deploy:2022.09
+docker push cimg/deploy:2022.09.1
+docker push cimg/deploy:2022.09-node
+docker push cimg/deploy:2022.09.1-node

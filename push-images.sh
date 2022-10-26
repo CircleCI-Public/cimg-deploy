ab5d7d45c22afaa48fa1fb7042adbbb09bc2bd23
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-26 12:34:42 -0400

    Publish v2022.10.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/deploy:2022.09
-docker push cimg/deploy:2022.09.1
-docker push cimg/deploy:2022.09-node
-docker push cimg/deploy:2022.09.1-node
+docker push cimg/deploy:2022.10
+docker push cimg/deploy:2022.10.1
+docker push cimg/deploy:2022.10-node
+docker push cimg/deploy:2022.10.1-node

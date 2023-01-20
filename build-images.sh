78e18ef6e1877c1f9cc2a239c04d21e0cdb6b2f7
Jeff Chen <jeff.chen@circleci.com>
2023-01-20 11:27:03 -0800

    Publish v2023.01. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.11/Dockerfile -t cimg/deploy:2022.11.1 -t cimg/deploy:2022.11 .
-docker build --file 2022.11/node/Dockerfile -t cimg/deploy:2022.11.1-node -t cimg/deploy:2022.11-node .
+docker build --file 2023.01/Dockerfile -t cimg/deploy:2023.01 -t cimg/deploy:2023.01 .
+docker build --file 2023.01/node/Dockerfile -t cimg/deploy:2023.01-node -t cimg/deploy:2023.01-node .

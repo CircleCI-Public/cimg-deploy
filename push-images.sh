110e7d270bc53101edd9db6b0c4be69e44613081
Jeff Chen <jeff.chen@circleci.com>
2023-01-23 11:35:24 -0800

    Publish v2023.01.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/deploy:2022.11
-docker push cimg/deploy:2022.11.1
-docker push cimg/deploy:2022.11-node
-docker push cimg/deploy:2022.11.1-node
+docker push cimg/deploy:2023.01
+docker push cimg/deploy:2023.01.1
+docker push cimg/deploy:2023.01-node
+docker push cimg/deploy:2023.01.1-node

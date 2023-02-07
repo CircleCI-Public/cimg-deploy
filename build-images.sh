899563254fbabcffec5be0679f7184d4e29bf1d3
Ricardo N Feliciano <FelicianoTech@gmail.com>
2023-02-07 12:54:24 -0500

    Publish v2023.02.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.01/Dockerfile -t cimg/deploy:2023.01.1 -t cimg/deploy:2023.01 .
-docker build --file 2023.01/node/Dockerfile -t cimg/deploy:2023.01.1-node -t cimg/deploy:2023.01-node .
+docker build --file 2023.02/Dockerfile -t cimg/deploy:2023.02.1 -t cimg/deploy:2023.02 .
+docker build --file 2023.02/node/Dockerfile -t cimg/deploy:2023.02.1-node -t cimg/deploy:2023.02-node .

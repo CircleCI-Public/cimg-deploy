12ee9a7f155acf9a1065de62d69031a0d49b416d
Adam Hartley <BytesGuy@users.noreply.github.com>
2023-04-03 18:26:17 +0100

    Publish v2023.04.1. [release] (#28)
    * Publish v2023.04.1. [release]
    
    * Publish v2023.04.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.03/Dockerfile -t cimg/deploy:2023.03.1 -t cimg/deploy:2023.03 .
-docker build --file 2023.03/node/Dockerfile -t cimg/deploy:2023.03.1-node -t cimg/deploy:2023.03-node .
+docker build --file 2023.04/Dockerfile -t cimg/deploy:2023.04.1 -t cimg/deploy:2023.04 .
+docker build --file 2023.04/node/Dockerfile -t cimg/deploy:2023.04.1-node -t cimg/deploy:2023.04-node .

4d3aa341b98f6dda66c453f4fdc1d42eb0013291
Adam Hartley <BytesGuy@users.noreply.github.com>
2023-05-03 20:03:35 +0100

    Publish v2023.05.1. [release] (#29)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.04/Dockerfile -t cimg/deploy:2023.04.1 -t cimg/deploy:2023.04 .
-docker build --file 2023.04/node/Dockerfile -t cimg/deploy:2023.04.1-node -t cimg/deploy:2023.04-node .
+docker build --file 2023.05/Dockerfile -t cimg/deploy:2023.05.1 -t cimg/deploy:2023.05 .
+docker build --file 2023.05/node/Dockerfile -t cimg/deploy:2023.05.1-node -t cimg/deploy:2023.05-node .

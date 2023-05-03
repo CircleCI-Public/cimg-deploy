53e497ec58aecf65fe8163991dc6b39a06275706
Adam Hartley <git@ahartley.com>
2023-05-03 20:00:37 +0100

    Publish v2023.05.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/deploy:2023.04
-docker push cimg/deploy:2023.04.1
-docker push cimg/deploy:2023.04-node
-docker push cimg/deploy:2023.04.1-node
+docker push cimg/deploy:2023.05
+docker push cimg/deploy:2023.05.1
+docker push cimg/deploy:2023.05-node
+docker push cimg/deploy:2023.05.1-node

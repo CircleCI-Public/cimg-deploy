528a7f666cc1d21984868826f8b8e919bbd8187b
Adam Hartley <git@ahartley.com>
2023-06-04 22:19:16 +0100

    Publish v2023.06.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.05/Dockerfile -t cimg/deploy:2023.05.1 -t cimg/deploy:2023.05 .
-docker build --file 2023.05/node/Dockerfile -t cimg/deploy:2023.05.1-node -t cimg/deploy:2023.05-node .
+docker build --file 2023.06/Dockerfile -t cimg/deploy:2023.06.1 -t cimg/deploy:2023.06 .
+docker build --file 2023.06/node/Dockerfile -t cimg/deploy:2023.06.1-node -t cimg/deploy:2023.06-node .

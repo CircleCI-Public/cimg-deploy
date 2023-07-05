df5fc54ac0d8e118e09a8a7b7b9308d0db639664
ryanbourdais <ryanbourdais@outlook.com>
2023-07-05 11:30:07 -0500

    Publish v2023.07. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.06/Dockerfile -t cimg/deploy:2023.06.1 -t cimg/deploy:2023.06 .
-docker build --file 2023.06/node/Dockerfile -t cimg/deploy:2023.06.1-node -t cimg/deploy:2023.06-node .
+docker build --file 2023.07/Dockerfile -t cimg/deploy:2023.07 -t cimg/deploy:2023.07 .
+docker build --file 2023.07/node/Dockerfile -t cimg/deploy:2023.07-node -t cimg/deploy:2023.07-node .

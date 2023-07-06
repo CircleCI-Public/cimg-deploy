de78a10802a040b28e6b6fd9fd9ced5422cd2a9b
Ryan Bourdais <67397011+ryanbourdais@users.noreply.github.com>
2023-07-06 11:16:08 -0500

    Publish v2023.07. [release] (#33)

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/deploy:2023.06
-docker push cimg/deploy:2023.06.1
-docker push cimg/deploy:2023.06-node
-docker push cimg/deploy:2023.06.1-node
+docker push cimg/deploy:2023.07
+docker push cimg/deploy:2023.07-node

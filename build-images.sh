64111b3cde5d34885e0c80577c32d3bb6c362e23
Adam Hartley <git@ahartley.com>
2023-07-19 10:46:19 +0100

    Test build

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+set -eo pipefail
 
-docker build --file 2023.07/Dockerfile -t cimg/deploy:2023.07 -t cimg/deploy:2023.07 .
-docker build --file 2023.07/node/Dockerfile -t cimg/deploy:2023.07-node -t cimg/deploy:2023.07-node .
+docker context create cimg
+docker buildx create --use cimg
+docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.07/Dockerfile -t cimg/deploy:2023.07 -t cimg/deploy:2023.07 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.07/node/Dockerfile -t cimg/deploy:2023.07-node -t cimg/deploy:2023.07-node --push .

d56d394281d0521c134813f74e2aa0dc840d02fd
Adam Hartley <git@ahartley.com>
2023-07-20 10:08:27 +0100

    Publish v2023.07.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/deploy:2023.07
-docker push cimg/deploy:2023.07-node
+set -eo pipefail

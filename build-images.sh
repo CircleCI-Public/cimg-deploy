3f50f882e03348a0483cc6ae25f9d1ac4147534f
Adam Hartley <git@ahartley.com>
2023-08-02 14:31:41 +0100

    Publish v2023.08.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.07/Dockerfile -t cimg/deploy:2023.07.1 -t cimg/deploy:2023.07 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.07/node/Dockerfile -t cimg/deploy:2023.07.1-node -t cimg/deploy:2023.07-node --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.08/Dockerfile -t cimg/deploy:2023.08.1 -t cimg/deploy:2023.08 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.08/node/Dockerfile -t cimg/deploy:2023.08.1-node -t cimg/deploy:2023.08-node --push .

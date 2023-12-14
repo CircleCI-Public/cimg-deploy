5c81090e6fa0d9abf1f150a8e3bc9f3bfec42dfe
Jeff Chen <jeff.chen@circleci.com>
2023-12-14 11:23:03 -0800

    Publish v2023.12.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.09/Dockerfile -t cimg/deploy:2023.09.1 -t cimg/deploy:2023.09 --push .
-docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.09/node/Dockerfile -t cimg/deploy:2023.09.1-node -t cimg/deploy:2023.09-node --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.12/Dockerfile -t cimg/deploy:2023.12.1 -t cimg/deploy:2023.12 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 2023.12/node/Dockerfile -t cimg/deploy:2023.12.1-node -t cimg/deploy:2023.12-node --push .
